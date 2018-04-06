a2573b2c43863cc8c8285b98a384a9d3ac3f1daf
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@accenture.com>
2018-04-06 11:42:17 +0530

    removed # lines

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -1,12 +1,7 @@
 package org.ravi.helloworld;
-
 public class Hello {
 	
-#	public String displayHelloMessage(String message){
-#		message = "hi";
-#		return message;
 public static void main (String[] args) {
 	throw new UnsupportedOperationException ();
 }
-
 }
